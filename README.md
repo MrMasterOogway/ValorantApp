2b2a8dc575511a7a09c1fc1478954b951b987717
MrMasterOogway <yashraj@live.ca>
2025-12-07 14:38:58 -0700

    relocated files

--- README.md
+++
@@ -1,2 +0,0 @@
-# ValorantApp
-a valorant draft application that acts like a league of legends style draft pick
--- android/app/src/main/kotlin/com/example/valorantdraftapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.valorantdraftapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
